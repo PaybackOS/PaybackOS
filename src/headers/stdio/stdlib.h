87136bfafe7f629c78ab370b69e1a861d7eb1b55
ThatOSDeveloper <owen.winchell@proton.me>
2024-09-20 19:19:37 -0400

    added panic to stdlib.h

--- src/headers/stdio/stdlib.h
+++ src/headers/stdio/stdlib.h
@@ -4,9 +4,19 @@
 
 #include <stddef.h>
 
-void	*malloc(size_t);				//< The standard function.
-void	*realloc(void *, size_t);		//< The standard function.
-void	*calloc(size_t, size_t);		//< The standard function.
-void	free(void *);					//< The standard function.
+/*
+The memory allocation functions
+*/
+
+// Reserve memory of a certain size to a pointer
+void *malloc(size_t);
+// Resize a pointer
+void *realloc(void *, size_t);
+// Allocates memory for an array of size elements, each of size bytes, and initializes all elements to 0.
+void *calloc(size_t, size_t);
+// Free the memory of a pointer
+void free(void *);
+
+void panic(int error);
 
 #endif
