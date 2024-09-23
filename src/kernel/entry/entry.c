0a3781bd8ce4b4f86289891dd9a52a4358ad3e04
ThatOSDeveloper <owen.winchell@proton.me>
2024-09-23 16:56:43 -0400

    Removed some bloat

--- src/kernel/entry/entry.c
+++ src/kernel/entry/entry.c
@@ -1,5 +1,4 @@
 #include <tty.h>
-#include <stty.h>
 #include <string.h>
 #include <boot/multiboot.h>
 #include <boot/kernel.h>
@@ -10,28 +9,6 @@
 void _init(multiboot_info_t *info) {
     // Initialize the VGA text mode terminal
     terminal_initialize();
-    // Initialize the serial port (not needed in qemu)
-    init_serial();
-    // Print our hello to our init
-    print("Hello _init\n");
 
-    // Check if the cmdline field is available
-    if (info->flags & MULTIBOOT_INFO_CMDLINE) {
-        char *cmdline = (char *)info->cmdline;
-
-        // Check for "debug" in the cmdline using strstr
-        if (strstr(cmdline, "debug")) {
-            // Set debug to true
-            setdebug(1);
-            main();
-        } else {
-            // Set debug to false
-            setdebug(0);
-            main();
-        }
-    } else {
-        // Set debug to false
-        setdebug(0);
-        main();
-    }
+    main();
 }
--- src/kernel/entry/kernel.c
+++ src/kernel/entry/kernel.c
@@ -6,13 +6,6 @@
 void main() {
     // Print out our welcome message
     printf("Thank you for using PaybackOS\n");
-    if(debug()) {
-        // Test liballoc
-        void* testalloc = malloc(5);
-        strcpy(testalloc, "hi!\n");
-        printf("liballoc test: %s", testalloc);
-        free(testalloc);
-    }
     // Halt the CPU with our shutdown interrupt
     asm("int $0");
     // In the case of cli and hlt failing we reboot
