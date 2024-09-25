e8843eb23f356f3b29774028fa3b43a8a498fb06
ThatOSDeveloper <owen.winchell@proton.me>
2024-09-25 17:28:40 -0400

    Started to implement memcpy

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -1,10 +1,12 @@
 #include <tty.hpp>
 #include <liballoc.h>
+#include <string.h>
 
 void init_gdt();
 
 extern "C" void _init() {
     terminal_initialize();
     init_gdt();
+    void* ptrtest = malloc(22);
     print("Hello kernel world!\n");
 }
