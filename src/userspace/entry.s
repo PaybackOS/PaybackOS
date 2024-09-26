f961127411c9544e1707178cbbf4c0419fdb21f8
ThatOSDeveloper <owen.winchell@proton.me>
2024-09-26 07:10:43 -0400

    attempted to fix code with u/mpetch fix to entry.s in src/userspace/

--- src/userspace/entry.s
+++ src/userspace/entry.s
@@ -2,17 +2,17 @@
 .extern userspace_c
 
 switch_to_user_mode:
-    mov $0x1B, %ax              // Load the data selector (for ring 3) into %ax
+    mov $0x23, %ax              // Load the data selector (for ring 3) into %ax
     mov %ax, %ds                // Move %ax to %ds
-    mov %ax, %es                // Move %ax to %es 
-    mov %ax, %fs                // Move %ax to %fs 
+    mov %ax, %es                // Move %ax to %es
+    mov %ax, %fs                // Move %ax to %fs
     mov %ax, %gs                // Move %ax to %gs // SS is handled by iret
 
     // Set up the stack frame iret expects
-    mov %eax, %esp              // Move the value of %eax to %esp
-    push $0x1B                  // Push the data selector
+    mov %esp, %eax              // Move the value of %esp to %eax
+    push $0x23                  // Push the data selector
     push %eax                   // Push current esp
     pushf                       // Push flags
-    push $0x23                  // Push the code selector (for ring 3)
-    push userspace_c           // Push instruction address to return to
+    push $0x1b                  // Push the code selector (for ring 3)
+    push $userspace_c           // Push instruction address to return to
     iret
