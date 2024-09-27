268e024045fbbccaa482c432313258a83d281ca5
ThatOSDeveloper <owen.winchell@proton.me>
2024-09-27 19:01:49 -0400

    Minor bug fix to printing to actually fix the scrolling function.

--- src/stdio/print.cpp
+++ src/stdio/print.cpp
@@ -30,6 +30,7 @@ static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
     return (uint16_t)uc | (uint16_t)color << 8;
 }
+
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 
@@ -39,19 +40,20 @@ uint8_t terminal_color;
 uint16_t* terminal_buffer;
 
 void update_cursor(int x, int y) {
-	uint16_t pos = y * VGA_WIDTH + x;
+    uint16_t pos = y * VGA_WIDTH + x;
 
-	outb(0x3D4, 0x0F);
-	outb(0x3D5, (uint8_t) (pos & 0xFF));
-	outb(0x3D4, 0x0E);
-	outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (uint8_t) (pos & 0xFF));
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
 }
-void enable_cursor(uint8_t cursor_start, uint8_t cursor_end) {
-	outb(0x3D4, 0x0A);
-	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
 
-	outb(0x3D4, 0x0B);
-	outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end);
+void enable_cursor(uint8_t cursor_start, uint8_t cursor_end) {
+    outb(0x3D4, 0x0A);
+    outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
+
+    outb(0x3D4, 0x0B);
+    outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end);
 }
 
 void terminal_initialize(void) {
@@ -59,7 +61,7 @@ void terminal_initialize(void) {
     terminal_column = 0;
     terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
     terminal_buffer = (uint16_t*)0xB8000;
-	update_cursor(terminal_column, terminal_row);
+    update_cursor(terminal_column, terminal_row);
     for (size_t y = 0; y < VGA_HEIGHT; y++) {
         for (size_t x = 0; x < VGA_WIDTH; x++) {
             const size_t index = y * VGA_WIDTH + x;
@@ -67,54 +69,58 @@ void terminal_initialize(void) {
         }
     }
 }
+
 namespace vga {
     void terminal_setcolor(uint8_t color) {
         terminal_color = color;
     }
-    
+
     void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
         const size_t index = y * VGA_WIDTH + x;
         terminal_buffer[index] = vga_entry(c, color);
     }
-    
-    // New function to handle scrolling
+
+    // Function to handle scrolling
     void terminal_scroll() {
-        // Clear the last row
+        // Move all lines one row up
+        memmove(terminal_buffer, terminal_buffer + VGA_WIDTH, (VGA_HEIGHT - 1) * VGA_WIDTH * sizeof(uint16_t));
+
+        // Clear the last line
         for (size_t x = 0; x < VGA_WIDTH; x++) {
             const size_t index = (VGA_HEIGHT - 1) * VGA_WIDTH + x;
             terminal_buffer[index] = vga_entry(' ', terminal_color);
         }
-        
-        // Shift contents up
-        memmove(terminal_buffer, terminal_buffer + VGA_WIDTH, (VGA_HEIGHT - 1) * VGA_WIDTH * sizeof(uint16_t));
+
+        // Move cursor to the last row
+        terminal_row = VGA_HEIGHT - 1;
+        terminal_column = 0;
+        update_cursor(terminal_column, terminal_row);
     }
-    
+
     void putchar(char c) {
-    	if (c == '\n') {
-    		terminal_row++;
-    		terminal_column = 0;
-    		update_cursor(terminal_column, terminal_row);
-    		return;
-    	}
-        terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-        
-        // Check if we've reached the end of the screen
-        if (++terminal_column == VGA_WIDTH) {
+        if (c == '\n') {
             terminal_column = 0;
-            
-            // Scroll up if we're at the bottom of the screen
             if (++terminal_row == VGA_HEIGHT) {
                 terminal_scroll();
             }
+        } else {
+            terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+            if (++terminal_column == VGA_WIDTH) {
+                terminal_column = 0;
+                if (++terminal_row == VGA_HEIGHT) {
+                    terminal_scroll();
+                }
+            }
         }
-    	update_cursor(terminal_column, terminal_row);
+        update_cursor(terminal_column, terminal_row);
     }
-    
+
     void terminal_write(const char* data, size_t size) {
-        for (size_t i = 0; i < size; i++)
+        for (size_t i = 0; i < size; i++) {
             putchar(data[i]);
+        }
     }
-    
+
     void print(const char* data) {
         while (*data != '\0') {
             putchar(*data);
