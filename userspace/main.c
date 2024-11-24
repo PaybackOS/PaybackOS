46e6f0daa4be3340b4bc9256ae559d00940caf11
ThatOSDeveloper <owen.winchell@proton.me>
2024-11-24 17:11:22 -0500

    added in scheduling test.

--- userspace/main.c
+++ userspace/main.c
@@ -3,13 +3,18 @@
 #include <task.h>
 
 void task1() {
-    
+    putchar('A');
+    yield();
+}
+
+void task2() {
+    putchar('B');
+    yield();
 }
 
 void userspace_init(void) {
-    if (checkdebug() == true) {
-        asm("cli");
-        log(1, "This is a message from the userspace");
-    }
+    start_task(task1);
+    start_task(task2);
+    scheduler();
     while (1);
 }
