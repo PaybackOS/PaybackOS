1589d9eef613d5b4e19c91e42b69d470e4994846
ThatOSDeveloper <owen.winchell@proton.me>
2024-12-14 13:14:01 -0500

    (nonworking) attempt at using SSFN

---
+++ kernel/headers/port.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdint.h>
+
+void outb(uint16_t port, uint8_t val);
+uint8_t inb(uint16_t port);
---
+++ kernel/src/drivers/io.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+void outb(uint16_t port, uint8_t val) {
+    __asm__ volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+    /* There's an outb %al, $imm8 encoding, for compile-time constant port numbers that fit in 8b. (N constraint).
+     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
+     * The  outb  %al, %dx  encoding is the only option for all other cases.
+     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
+}
+
+uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    __asm__ volatile ( "inb %w1, %b0"
+                   : "=a"(ret)
+                   : "Nd"(port)
+                   : "memory");
+    return ret;
+}
