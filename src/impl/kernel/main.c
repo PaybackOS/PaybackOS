90201b53d9bcc1cf53bccecd5d51c9f3d27b38f1
ThatOSDeveloper <owen.winchell@proton.me>
2024-12-14 21:44:32 -0500

    make setup not use docker

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,7 +1,7 @@
-#include "print.h"
+#include <print.h>
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to our 64-bit kernel!");
+    print_set_color(PRINT_COLOR_LIGHT_GRAY, PRINT_COLOR_BLACK);
+    print_str("Welcome to our 64-bit kernel!\nTest");
 }
